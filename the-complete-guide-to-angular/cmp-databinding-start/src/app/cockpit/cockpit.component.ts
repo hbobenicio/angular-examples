2b61a7d63415cdcaa3b70949503fc623353ffaea
Hugo Benicio <hbobenicio@gmail.com>
2017-11-10 16:52:36 -0300

    finishing section 05 - before assignment

--- the-complete-guide-to-angular/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ the-complete-guide-to-angular/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,28 +6,42 @@ import { Component, OnInit, EventEmitter } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-  serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
-  newServerName = '';
-  newServerContent = '';
+  // The @Output is used to expose a Event Out interface to the users of this component.
+  // The EventEmitter property can be used to emit them.
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+  // newServerName = '';
+  // newServerContent = '';
+  // The @ViewChild is used to get a Local Reference to a DOM Element from the Template
+  @ViewChild('serverContentElement') serverContentElement: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer() {
+  // Passing a Local Reference to a DOM Element By Parameter
+  onAddServer(serverNameElement: HTMLInputElement) {
+    // this.serverCreated.emit({
+    //   serverName: serverNameElement.value,
+    //   serverContent: this.newServerContent
+    // });
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: serverNameElement.value,
+      serverContent: this.serverContentElement.nativeElement.value // Using a Local Reference to a DOM Element by Property
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(serverNameElement: HTMLInputElement) {
+    // this.blueprintCreated.emit({
+    //   serverName: serverNameElement.value,
+    //   serverContent: this.newServerContent
+    // });
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: serverNameElement.value,
+      serverContent: this.serverContentElement.nativeElement.value
     });
   }
 
--- the-complete-guide-to-angular/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ the-complete-guide-to-angular/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,16 +1,79 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  DoCheck,
+  SimpleChanges,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild, ContentChild, ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None // This will make the server-element.component.css rules global
 })
-export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+export class ServerElementComponent implements
+  OnInit, OnChanges, DoCheck,
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked,
+  OnDestroy {
 
-  constructor() { }
+  @Input() element: {type: string, name: string, content: string};
+  @Input() name: string;
+
+  // To access a Local Reference to a DOM Element from the template
+  @ViewChild('heading') header: ElementRef;
+
+  // To access a Local Reference to a DOM Element from a projected ng-content (client Element Body)
+  @ContentChild('contentParagraph') paragraph: ElementRef;
+
+  constructor() {
+    console.log('ServerElementComponent.constructor called!');
+  }
 
   ngOnInit() {
+    console.log('ServerElementComponent.ngOnInit called!');
+    console.log('[ngOnInit] Heading Content: ' + this.header.nativeElement.textContent);
+    console.log('[ngOnInit] paragraph Content: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ServerElementComponent.ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngDoCheck() {
+    console.log('ServerElementComponent.ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ServerElementComponent.ngAfterContentInit called!');
+    console.log('[ngAfterContentInit] paragraph Content: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked() {
+    console.log('ServerElementComponent.ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit() {
+    console.log('ServerElementComponent.ngAfterViewInit called!');
+    console.log('[ngAfterViewInit] Heading Content: ' + this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    console.log('ServerElementComponent.ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log('ServerElementComponent.ngOnDestroy caleed!');
   }
 
 }
